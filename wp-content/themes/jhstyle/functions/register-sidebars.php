5575ff15e752b5d637b68d7dc51153c771f7d7ad
Stephen Williams <stephen@wearetmbr.com>
2017-07-19 20:12:28 -0600

    functions

--- wp-content/themes/jhstyle/functions/register-sidebars.php
+++ wp-content/themes/jhstyle/functions/register-sidebars.php
@@ -12,23 +12,3 @@ register_sidebar( array(
     'before_title' => '<h5 class="widget-title">',
     'after_title' => '</h5>',
 ) );
-
-register_sidebar( array(
-    'name' => 'Footer Column 2',
-    'id' => 'footer-column-2',
-    'description' => 'Appears in the footer area',
-    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-    'after_widget' => '</aside>',
-    'before_title' => '<h5 class="widget-title">',
-    'after_title' => '</h5>',
-) );
-
-register_sidebar( array(
-    'name' => 'Footer Column 3',
-    'id' => 'footer-column-3',
-    'description' => 'Appears in the footer area',
-    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-    'after_widget' => '</aside>',
-    'before_title' => '<h5 class="widget-title">',
-    'after_title' => '</h5>',
-) );
