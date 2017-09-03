762b8309614cc3dbd24ad542bd2210133224e944
Stephen Williams <stephen@wearetmbr.com>
2017-09-03 08:42:33 -0600

    advertisers

--- wp-content/themes/jhstyle/partials/content/advertiser-slider.php
+++ wp-content/themes/jhstyle/partials/content/advertiser-slider.php
@@ -1,6 +1,38 @@
 <div class="container normal-pad">
     <div class="row">
-    <h2 class="center">Advertisers</h2>
-        
+    	<h2 class="center">Advertisers</h2>
+        <?php
+    		$advertiser_loop = new WP_Query( array(
+        		'post_type' => 'advertisers',
+        		'orderby' => 'title',
+				'order'   => 'DESC',
+        		'posts_per_page' => -1
+    		) );
+		?>
+
+    	<div id="advertiser-slider" class="flexslider advertiser-slider">
+	        <ul class="slides">
+	        <?php
+	        while ( $advertiser_loop->have_posts() ) {
+	            $advertiser_loop->the_post();
+	            $image_id = get_post_thumbnail_id();
+	            list( $url, $width, $height ) = wp_get_attachment_image_src( $image_id, 'medium' );
+	            $advertiserLink = get_field('advertiser_link');
+	            
+	            ?>
+	                <li>
+	                    <a href="<?php echo $advertiserLink ?>" target="_blanks">
+	                    	<div class="advertiser-logo">
+	                        	<img src="<?php echo $url; ?>" alt="<?php the_title();?>">
+	                        	<h3><?php the_title();?></h3>
+	                    	</div>    
+	                    </a>
+	                </li>
+	            <?php
+	        }
+	        wp_reset_postdata();
+	        ?>
+	        </ul>
+	    </div>
     </div>
 </div>
