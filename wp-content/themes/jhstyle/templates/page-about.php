5ec22d4a7007bf693d9901e2f4338bc742b73dc5
Stephen Williams <stephen@wearetmbr.com>
2017-09-09 14:32:46 -0600

    about page template

---
+++ wp-content/themes/jhstyle/templates/page-about.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: About/Contact
+*/
+
+get_header(); ?>
+
+<!-- PAGE HERO -->
+
+<?php get_template_part('partials/global/page-hero');?>
+
+<div class="page-content"> 
+    <div class="container">
+        <div class="row">
+
+
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part('partials/global/page-content'); ?>
+		<?php endwhile; // end of the loop. ?>
+
+		<?php get_template_part('partials/loops/team-loop'); ?>
+
+		</div>
+	</div>
+</div>
+
+
+
+
+<?php get_footer();?>
