5f9e84c5bea38789eaeb5a4f5d20eb1fb1032ae0
Stephen Williams <stephen@wearetmbr.com>
2017-09-10 14:00:02 -0600

    team loop

---
+++ wp-content/themes/jhstyle/partials/loops/team-loop.php
@@ -0,0 +1,22 @@
+<?php if( have_rows('team_member') ): while ( have_rows('team_member') ) : the_row(); 
+
+$teamPhoto = get_sub_field('team_photo');
+$teamName = get_sub_field('team_name');
+$teamPosition = get_sub_field('team_position');
+$teamEmail = get_sub_field('team_email');
+$teamDescription = get_sub_field('team_description');
+
+?>
+
+     <div class="col-sm-4">
+     	<div class="team-wrap">
+	     <img src="<?php echo $teamPhoto['sizes']['cover'];?>" alt="<?php echo $teamName;?>"/>
+	     <h3><?php echo $teamName;?></h3>
+	     <h5><?php echo $teamPosition;?></h5>
+	     <h5><span><i class="fa fa-fw fa-envelope"></i> <?php echo $teamEmail;?></span></h5>
+	     <p><?php echo $teamDescription; ?></p>
+	    </div>
+     </div>
+
+
+<?php endwhile; endif; ?>
--- wp-content/themes/jhstyle/templates/page-about.php
+++ wp-content/themes/jhstyle/templates/page-about.php
@@ -13,14 +13,16 @@ get_header(); ?>
     <div class="container">
         <div class="row">
 
-
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part('partials/global/page-content'); ?>
 		<?php endwhile; // end of the loop. ?>
 
-		
-
 		</div>
+
+		<div class="row">
+			<?php get_template_part('partials/loops/team-loop'); ?>
+		</div>
+
 	</div>
 </div>
 
