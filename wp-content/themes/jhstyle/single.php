36e6618395fb9b666af0c2d989dd7df04cc7b76b
Stephen Williams <stephen@wearetmbr.com>
2017-09-15 20:10:04 -0600

    post settings acf

--- wp-content/themes/jhstyle/single.php
+++ wp-content/themes/jhstyle/single.php
@@ -9,6 +9,13 @@ if( !empty($terms) ) {
 	$categoryImage = get_field('category_featured_image', $term );
 }
 
+	$writer = get_field('ps_written_by');
+	$photographer = get_field('ps_photography_by');
+	$subtitle = get_field('ps_subtitle');
+	$issue = get_field('ps_issue');
+	$image_id = get_post_thumbnail_id();
+    list( $url, $width, $height ) = wp_get_attachment_image_src( $image_id, 'lg_thumb' );
+
 ?>
 
 <div class="container-fluid nopad">
@@ -26,6 +33,33 @@ if( !empty($terms) ) {
 
         	<div class="col-md-10 col-md-offset-1">
 				<?php while ( have_posts() ) : the_post(); ?>
+
+					<div class="row">
+						
+						<div class="col-sm-6">
+							<img src="<?php echo $url;?>" alt="<?php the_title();?>"/>
+						</div>
+
+						<div class="col-sm-6">
+							<div class="post-meta">
+								<h1><?php the_title();?></h1>
+								<h2><?php echo $subtitle;?></h2>
+								
+								<?php if($writer) { ?>
+									<h3>Written By: <?php echo $writer;?></h3>
+								<?php } ?>
+								
+								<?php if($photographer) { ?>
+									<h3>Photography By: <?php echo $photographer;?></h3>
+								<?php } ?>
+
+								<?php if($issue) { ?>
+									<h4><?php echo $issue;?></h4>
+								<?php } ?>
+							</div>
+						</div>
+
+					</div>
 
 					<?php the_content();?>
 
